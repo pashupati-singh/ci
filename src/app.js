ea46191f07f890a88f0b94e23e54e88b9985d1eb
coderoofsit <119448686+pashupati-singh@users.noreply.github.com>
2026-06-22 00:25:22 +0530

    update in app.js file

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 
 app.use(express.json());
+app.get("/",(req,res)=>{
+   res.send("server is running");
+})
 app.use('/api/users', userRoutes);
 
 module.exports = app;
